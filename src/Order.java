071b694f95bd069563254779c02e5995de513683
lenth <truocluong@gmail.com>
2019-02-15 08:24:35 +0700

    update lần cuối 1

--- src/Order.java
+++ src/Order.java
@@ -4,20 +4,20 @@ public class Order {
   public double total;
   ArrayList<String> danhSachSP = new ArrayList<>();
   public void MuaSP(Ao ao,int soLuong){
-     if(soLuong<ao.amount){
+     if(soLuong<=ao.amount){
          danhSachSP.add(ao.name);
          ao.amount-=soLuong;
-         this.total+= ao.money;
+         this.total+= ao.money*soLuong;
      }
    else {
          System.out.println("Hình như có gì đấy sai sai ");
      }
   }
   public void MuaSP(Nhan nhan,int soLuong){
-      if(soLuong<nhan.amount){
+      if(soLuong<=nhan.amount){
           danhSachSP.add(nhan.name);
           nhan.amount-=soLuong;
-          this.total+= nhan.money;
+          this.total+= nhan.money*soLuong;
       }
       else {
           System.out.println("Hình như có gì đấy sai sai ");
